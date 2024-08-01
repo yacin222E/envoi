d81a87dc3a9a7a1c7be5285c814deb023b97481f
unknown <jacob220@hotmail.fr>
2024-08-01 17:54:55 +0200

    3 fichier de js html

---
+++ yacine.js
@@ -0,0 +1,7 @@
+let motTapeOk = true // Essayez de mettre false à la place de true
+
+if (motTapeOk) {
+    console.log("Bravo, vous avez correctement tapé le mot")
+} else {
+    console.log("Échec, le mot n'est pas correct")
+}
